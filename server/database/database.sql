360849efb07949f0333465bb2ee89331a74b5407
Ryan Barfield <cadium828@gmail.com>
2024-01-08 16:57:58 -0600

    updated the database.sql file with the queries made

--- server/database/database.sql
+++ server/database/database.sql
@@ -9,3 +9,20 @@ CREATE TABLE tasks (
 	priority VARCHAR(10) NOT NULL,
 	task_group VARCHAR(20)
 );
+
+-- Queries used FOR CRUD
+
+-- Create
+INSERT INTO tasks (task_name, due_date, priority, task_group)
+    VALUES($1, $2, $3, $4);
+
+-- Read
+SELECT * FROM tasks;
+
+-- Update
+UPDATE tasks
+        SET task_name = $1, due_date = $2, priority = $3, task_group = $4
+        WHERE id = $5;
+
+-- Delete
+DELETE FROM tasks WHERE id = $1;
