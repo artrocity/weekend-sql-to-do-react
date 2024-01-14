33d5ce7cbaf68e9a49532a1324c154b4f673fbd3
Ryan Barfield <cadium828@gmail.com>
2024-01-14 13:23:25 -0600

    updated tasklist.jsx with base functionality

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,16 @@
+// Import Modules
+import React from 'react';
+import TaskCard from './TaskCard';
+
+// Function to 
+function TaskList() {
+  // Fetch All Current Tasks from database
+
+    return (
+        <div className="task-list">
+        </div>
+    );
+}
+
+// Export Tasks
+export default TaskList;
