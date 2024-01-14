9f508b19b62c46e077e5fb2c5543442f91e73743
Ryan Barfield <cadium828@gmail.com>
2024-01-14 13:21:23 -0600

    updated navbar.jsx with base functionality

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+// Import modules
+import React from 'react';
+
+// Function to create the sidenavbar
+function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">To-Do App</span>
+        </div>
+        </nav>
+    );
+}
+
+// Export the navbar function
+export default Navbar;
