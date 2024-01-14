767e19a6586d693878e1af82876c3db6e42f8f24
Ryan Barfield <cadium828@gmail.com>
2024-01-14 13:33:21 -0600

    updated themetoggle.jsx with base functionality

---
+++ src/components/TaskForm/TaskForm.jsx
@@ -0,0 +1,14 @@
+// Import modules
+import React from 'react';
+
+// Create the function for the add task form
+function TaskForm() {
+    return (
+        <form>
+        
+        </form>
+    );
+};
+
+// Export function to app
+export default TaskForm;
