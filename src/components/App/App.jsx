fce030c6b600e586a2cd8ac984ba03b0d2a83893
Ryan Barfield <cadium828@gmail.com>
2024-01-14 13:20:04 -0600

    updated app.jsx with base functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,23 @@
-import {useState} from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
+import TaskList from './TaskList';
+import ThemeToggle from './ThemeToggle';
 
-function App () {
+function App() {
+  const [theme, setTheme] = useState('dark'); // 'light' or 'dark'
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
 
     return (
-        <div>
-        <h1>TO DO APP</h1>
+        <div className={`App ${theme}`}>
+        <Navbar />
+        <ThemeToggle toggleTheme={toggleTheme} />
+        <TaskList />
         </div>
     );
-
 }
 
-export default App
+export default App;
